f961103dc4eb2efab3ba7616e9e598e1ec5a5d4a
Marc <ninjaz.gg@outlook.com>
2023-10-01 18:04:23 -0400

    get epub

--- src/Components/ReaderBody.svelte
+++ src/Components/ReaderBody.svelte
@@ -1,16 +1,28 @@
 <script>
+	import { onMount } from "svelte";
+
+	let arrayBuffer;
+
+	onMount(async () => {
+		const response = await fetch("http://localhost:8000/epubs/1");
+		arrayBuffer = await response.arrayBuffer();
+
+		const book = ePub(arrayBuffer);
+		const rendition = book.renderTo("viewer", { width: 600, height: 400 });
+		rendition.display();
+	});
 </script>
 
-<div></div>
+<div id="viewer" />
 
 <style>
-div {
-  position: fixed;
-  display: flex;
-  top: 0px;
-  left: 0px;
-  height: 100vh;
-  width: 100vw;
-  background-color: #363a3f;
-}
+	div {
+		position: fixed;
+		display: flex;
+		top: 0px;
+		left: 0px;
+		height: 100vh;
+		width: 100vw;
+		background-color: #363a3f;
+	}
 </style>
