8bb74be19314c3578f9a1e4328031ea0108482c5
Marc <ninjaz.gg@outlook.com>
2023-10-02 18:45:10 -0400

    test

--- src/Components/ReaderBody.svelte
+++ src/Components/ReaderBody.svelte
@@ -1,28 +1,34 @@
 <script>
-	import { onMount } from "svelte";
+  import { onMount } from "svelte";
 
-	let arrayBuffer;
+  let arrayBuffer;
+  
 
-	onMount(async () => {
-		const response = await fetch("http://localhost:8000/epubs/1");
-		arrayBuffer = await response.arrayBuffer();
+  onMount(async () => {
+    console.log("onMount called");
+    const response = await fetch("http://localhost:9000/api/epubs/1");
+    arrayBuffer = await response.arrayBuffer();
+    console.log(arrayBuffer);
 
-		const book = ePub(arrayBuffer);
-		const rendition = book.renderTo("viewer", { width: 600, height: 400 });
-		rendition.display();
-	});
+    var book = ePub(arrayBuffer);
+    const rendition = book.renderTo("viewer", { width: 600, height: 400 });
+    rendition.display();
+  });
 </script>
 
-<div id="viewer" />
+<div>
+  <div id="viewer" />
+  <div><p>test</p></div>
+</div>
 
 <style>
-	div {
-		position: fixed;
-		display: flex;
-		top: 0px;
-		left: 0px;
-		height: 100vh;
-		width: 100vw;
-		background-color: #363a3f;
-	}
+  div {
+    position: fixed;
+    display: flex;
+    top: 0px;
+    left: 0px;
+    height: 100vh;
+    width: 100vw;
+    background-color: #363a3f;
+  }
 </style>
