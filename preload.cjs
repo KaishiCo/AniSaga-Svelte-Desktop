5e3a246edb711975baa2b002891138dccd4f9850
Marc <ninjaz.gg@outlook.com>
2023-10-04 10:52:22 -0400

    run db initializer on preload

--- preload.cjs
+++ preload.cjs
@@ -1,1 +1,17 @@
 console.log("preload.js loaded");
+const sqlite3 = require("sqlite3").verbose();
+
+
+const db = new sqlite3.Database("./epub.db");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS epub (
+      id TEXT PRIMARY KEY,
+      title TEXT,
+      coverPath TEXT,
+      filePath TEXT,
+      pageLocation TEXT)
+  `);
+  db.close();
+});
