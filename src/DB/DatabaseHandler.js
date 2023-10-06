f1446598dc4787693f966c10b8011c2251b8cbb2
ArduousKokuhaku52 <arduouskokuhaku.gg@protonmail.com>
2023-10-06 04:43:54 -0500

    added getEpubInfo and insertEpub functions

--- src/DB/DatabaseHandler.js
+++ src/DB/DatabaseHandler.js
@@ -2,8 +2,22 @@ const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database("./epub.db");
 
-//insert
 
+//insert
+function insertEpub(id, title, coverPath, filePath) {
+    return new Promise((resolve, reject) => {
+        db.run(`INSERT INTO epub (id, title, coverPath, filePath, pageLocation)
+        VALUES ("`+id+`", "`+title+`", "`+coverPath+`", "`+filePath+`")
+        `, (err) => {
+            if (err) {
+                reject(false);
+            }
+            else {
+                resolve(true);
+            }
+        });
+    });
+}
 
 //delete
 
@@ -12,12 +26,28 @@ const db = new sqlite3.Database("./epub.db");
 
 
 //read
-async function getEpubs() {
-    db.run("SELECT * FROM epub", (err, rows) => {
-        if (err) throw err;
-
-        return rows;
+function getEpubs() {
+    return new Promise((resolve, reject) => {
+        db.all("SELECT * FROM epub", (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
     });
 }
 
-export { getEpubs };
+function getEpubInfo(id) {
+    return new Promise((resolve, reject) => {
+        db.get(`SELECT filePath, pageLocation FROM epub WHERE id="`+id+`"`, (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(row);
+            }
+        });
+    });
+}
+
+export { getEpubs, getEpubInfo };
