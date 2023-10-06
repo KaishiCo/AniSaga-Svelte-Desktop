c7c12a478b015f8828cbd8ff5064c042903d107b
ArduousKokuhaku52 <arduouskokuhaku.gg@protonmail.com>
2023-10-06 04:41:10 -0500

    now loads epub and changes to readerview on click

--- src/Components/BookCardCover.svelte
+++ src/Components/BookCardCover.svelte
@@ -1,11 +1,22 @@
 <script>
     export let imgurl;
     export let bookName;
-    export let pageLocation;
+    export let bookId;
+    import * as db from "../DB/DatabaseHandler";
+    import currentBookData from "../Stores/Book";
+    import { view } from "../Stores/View";
+
+    async function setBookInfo() {
+        let bookInfo = await db.getEpubInfo(bookId).then((res) => {
+            return res;
+        });
+        currentBookData.set({bookID: bookId, bookFP: bookInfo.filePath, location: bookInfo.pageLocation});
+        view.set(1);
+    }
 </script>
 
 <div>
-<img src={imgurl} alt={bookName} width="255" height="350">
+<img src={imgurl} alt={bookName} width="255" height="350" on:click={setBookInfo}>
 </div>
 
 <style>
@@ -14,7 +25,9 @@
         display: flex;
         margin: 0.3rem;
         min-width: 255px;
+        max-width: 255px;
         min-height: 350px;
+        max-height: 350px;
         background-color: black;
     }
 </style>
